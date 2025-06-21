99e212f467393626394648babdca14988ccc8d07
Hannah B <a1838917@adelaide.com>
2025-06-21 05:16:54 +0000

    [automatic commit] 2025-06-21 05:16:54 UTC 119.15.75.28

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const axios = require('axios');
 require('dotenv').config();
 
 const DOG_API = 'https://dog.ceo/api/breeds/image/random';
 
 router.get('/', async function(req, res, next) {
   try{
-    const response = await axios.get(DOG_API);
+    const response = await fetch(DOG_API);
 
     res.render('index',{
       imageUrl: response.data.url,
