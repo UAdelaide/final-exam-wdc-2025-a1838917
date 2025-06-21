1a4385fa471dd4c17a9584238f78a30e82295c54
Hannah B <a1838917@adelaide.com>
2025-06-21 03:19:58 +0000

    [automatic commit] 2025-06-21 03:19:57 UTC 119.15.75.28

--- part1/app.js
+++ part1/app.js
@@ -20,7 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/api/dogs', async (req, res) => {
-    
+    try{
+        
+    }
 })
 
 app.use(express.static(path.join(__dirname, 'public')));
