1e399cabc8134663590ae2102200a2159780673a
Hannah B <a1838917@adelaide.com>
2025-06-21 05:17:15 +0000

    [automatic commit] 2025-06-21 05:17:15 UTC 119.15.75.28

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -7,6 +7,7 @@ const DOG_API = 'https://dog.ceo/api/breeds/image/random';
 router.get('/', async function(req, res, next) {
   try{
     const response = await fetch(DOG_API);
+    const data = await response.json()
 
     res.render('index',{
       imageUrl: response.data.url,
