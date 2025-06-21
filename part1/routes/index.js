fae9a4b2cca9c3293fe5f80bf1086ec0dafbac2f
Hannah B <a1838917@adelaide.com>
2025-06-21 04:56:05 +0000

    [automatic commit] 2025-06-21 04:56:05 UTC ;; communications error to 208.67.222.222#53: timed out 119.15.75.28

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const DOG_API = 'https://dog.ceo/api/breeds/image/random'
+const DOG_API = 'https://dog.ceo/api/breeds/image/random';
+
+router.get()
 
 module.exports = router;
