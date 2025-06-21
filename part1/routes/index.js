e9f1869172aac91ff8e6b2aa8d37507fa773174f
Hannah B <a1838917@adelaide.com>
2025-06-21 05:47:45 +0000

    [automatic commit] 2025-06-21 05:47:45 UTC 119.15.75.28

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,21 +1,7 @@
 var express = require('express');
 var router = express.Router();
-require('dotenv').config();
-
-const DOG_API = 'https://dog.ceo/api/breeds/image/random';
 
 router.get('/', async function(req, res, next) {
-  try{
-    const response = await fetch(DOG_API);
-    const data = await response.json();
-
-    res.render('index',{
-      imageUrl: response.data.url,
-    });
-  }
-  catch(error){
-    console.error('Error: could not fetch dog photo', error.messege);
-  }
 });
 
 module.exports = router;
