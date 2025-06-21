b5e4fc4cf46950b9fae3cedac6daea2672de3823
Hannah B <a1838917@adelaide.com>
2025-06-21 03:03:04 +0000

    [automatic commit] 2025-06-21 03:03:04 UTC 119.15.75.28

--- part1/app.js
+++ part1/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const { fstat } = require('fs');
 
-const dv
+const db = new sqlite3.Database(':memory')
 
 app.use(logger('dev'));
 app.use(express.json());
