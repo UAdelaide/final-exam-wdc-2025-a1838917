e02b2803d2a42b73372d0ae13957f844e0c49a26
Hannah B <a1838917@adelaide.com>
2025-06-21 02:53:36 +0000

    [automatic commit] 2025-06-21 02:53:35 UTC 119.15.75.28

--- part1/app.js
+++ part1/app.js
@@ -14,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const db = fstat.readFileSync('dogwalks.sql','utf8');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
