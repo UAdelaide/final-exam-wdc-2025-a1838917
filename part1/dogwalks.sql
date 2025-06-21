261fb9f9ab37cc17dec698686a7cfe230b21fdc1
Hannah B <a1838917@adelaide.com>
2025-06-21 00:01:33 +0000

    [automatic commit] 2025-06-21 00:01:33 UTC 119.15.75.28

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -57,4 +57,4 @@ CREATE TABLE WalkRatings (
 INSERT INTO Users(user_id, username, email, password_hash, role)
 VALUES
 (1,alice123,alice@example.com,hashed123,owner)
-(2,bobwalker,)
+(2,bobwalker,bob@example.com,)
