2cf9ca270470631cb0ea0a1466e7c450a5d7c537
Hannah B <a1838917@adelaide.com>
2025-06-21 00:02:34 +0000

    [automatic commit] 2025-06-21 00:02:34 UTC 119.15.75.28

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -58,5 +58,4 @@ INSERT INTO Users(user_id, username, email, password_hash, role)
 VALUES
 (1,alice123,alice@example.com,hashed123,owner)
 (2,bobwalker,bob@example.com,hashed456,walker)
-(3,carol123,)
-(3,carol123,)
+(3,carol123,carol@example.com,)
