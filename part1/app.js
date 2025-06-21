31bb49b7e8a354d42da3f01e4abc7eed1534faa7
Hannah B <a1838917@adelaide.com>
2025-06-21 03:56:26 +0000

    [automatic commit] 2025-06-21 03:56:26 UTC 119.15.75.28

--- part1/app.js
+++ part1/app.js
@@ -36,7 +36,7 @@ app.get('/api/dogs', (req, res) => {
 });
 
 app.get('/api/walkrequests/open', (req, res) => {
-    const query = 'SELECT request_id, dog_name, requested_time, duration_minutes';
+    const query = 'SELECT request_id, dog_name, requested_time, duration_minutes, location, owner_id FROM ';
     pool.query(query, (err,results) => {
         if(err){
             console.error('error fetching dogs:',err);
