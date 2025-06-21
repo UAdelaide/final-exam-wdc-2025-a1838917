9d181c05edd81f8276fcb1ba4537e3ff55f83ac7
Hannah B <a1838917@adelaide.com>
2025-06-21 03:24:09 +0000

    [automatic commit] 2025-06-21 03:24:09 UTC 119.15.75.28

--- part1/app.js
+++ part1/app.js
@@ -17,7 +17,26 @@ const db = new sqlite3.Database('./dogwalks.db',(err) => {
         console.log('Loaded');
     }
 });
+(async () => {
+  try {
+    // Connect to MySQL without specifying a database
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '' // Set your MySQL root password
+    });
 
+    // Create the database if it doesn't exist
+    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.end();
+
+    // Now connect to the created database
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'testdb'
+    });
 
 
 app.use(logger('dev'));
