ab7a95057be23d44904b2ef67ae08136ace71e1a
Hannah B <a1838917@adelaide.com>
2025-06-21 02:37:03 +0000

    [automatic commit] 2025-06-21 02:37:03 UTC 119.15.75.28

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/api/dogs', function(req, res, next) {
 });
 
-router.get('/api/walkrequests/open')
+router.get('/api/walkrequests/open' , function(req, res, next){
+
+});
+
+
 module.exports = router;
